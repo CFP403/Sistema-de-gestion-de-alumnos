1a1cdeabc804e9f92115c0bf42c984fa7b0f56f7
LucasBois1 <44686060+LucasBois1@users.noreply.github.com>
2020-07-27 20:07:39 -0300

    Fix de carpetas en /src

---
+++ sistema de gestion/src/SistemaDeGestion/SistemaDeGestion.java
@@ -0,0 +1,14 @@
+
+
+package SistemaDeGestion;
+
+import Ventanas.VentanaPrincipal;
+
+public class SistemaDeGestion {
+
+    public static void main(String[] args) {
+        VentanaPrincipal main = new VentanaPrincipal();
+        main.Mostrar();
+    }
+
+}
